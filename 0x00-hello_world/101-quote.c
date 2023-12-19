4e586ce1852554a8041e27e0045c72706b229af7
chinazaonuoha <developerworkpace@gmail.com>
2023-12-19 05:47:39 +0100

    print to standard error fix 0x00

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  *main - print to the standard error.
  *
- *return: 1(error)
+ *Return: 1(error)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "\n");
 return (1);
 }
