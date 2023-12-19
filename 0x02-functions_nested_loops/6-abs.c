b726aa424cde103a121f96b8e4039276db97c0d2
chinazaonuoha <developerworkpace@gmail.com>
2023-12-19 22:56:05 +0100

    computes the absolute value of an integer fix 0x00

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *6-abs - print absolute number
+ *_abs - print absolute number
  *@n: integer param
  *
  *Return: 1
  */
 int _abs(int n)
 {
-if(n < 0)
+if (n < 0)
 {
 n = -n;
 }
