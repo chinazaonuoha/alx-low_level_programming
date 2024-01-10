82afdb6d399484f6a933159e773b29be6aebd6ef
chinazaonuoha <developerworkpace@gmail.com>
2024-01-10 04:00:58 +0100

    set pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,9 @@
+/**
+ *set_char - change pointer value to a character
+ *@s: a double pointer parameter
+ *@to: a character pointer
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 /** _strchr declaration*/
 char *_strchr(char *s, char c);
+/** set_string declaration*/
+void set_string(char **s, char *to);
