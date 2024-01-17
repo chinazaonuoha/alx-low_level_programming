bae6f88213b71745e119bcbff9baac0289591305
chinazaonuoha <developerworkpace@gmail.com>
2024-01-18 00:20:43 +0100

    creates an array of chars fix 0x00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *ptr;
+unsigned int i;
 if (size == 0)
 {
 return (NULL);
@@ -23,6 +24,10 @@ if (ptr == NULL)
 return (NULL);
 }
 ptr = (char *)malloc(sizeof(char) * size);
+
+for (i = 0; i < size; i++) {
+ptr[i] = c;
+}
 return (ptr);
 }
 }
