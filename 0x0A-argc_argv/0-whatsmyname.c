aaa718c13e413c3484339b87d1d02567ed9b7999
chinazaonuoha <developerworkpace@gmail.com>
2024-01-17 15:29:28 +0100

    Write a program that prints its name fix 0x00

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ *main - Gets the name of the program
+ *@argc: arugment count
+ *@argv: argument value
+ *
+ *Return: Integer
+ */
 int main(int argc, char *argv[])
 {
-if(argc > 0)
+if (argc > 0)
 {
 printf("%s\n", argv[0]);
 }
