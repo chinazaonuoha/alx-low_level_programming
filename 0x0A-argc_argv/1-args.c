d98854a2ee142259b8d4fe4f4e11196f812e1a90
chinazaonuoha <developerworkpace@gmail.com>
2024-01-17 15:55:00 +0100

    prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Gets the name of the program
+ *@argc: arugment count
+ *@argv: argument value
+ *
+ *Return: Integer
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+if (!(argc < 0))
+{
+printf("%d\n", argc - 1);
+}
+return (1);
+}
