33ad11cc9d5101efe004c2995761070211ea5b15
chinazaonuoha <developerworkpace@gmail.com>
2024-01-25 00:31:13 +0100

    header gaurd added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,10 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
 #include <stddef.h>
+
+/** _putchar - decleartion*/
+int _putchar(char c);
+
 /** print_name - decleartion*/
 void print_name(char *name, void (*f)(char *));
 
@@ -7,3 +13,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 /** int_index - decleartion*/
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
