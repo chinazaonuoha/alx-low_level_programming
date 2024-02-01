d953483011e82555539fff8582a344b070e2e976
chinazaonuoha <developerworkpace@gmail.com>
2024-02-01 14:53:46 +0100

    checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for a digit (0 through 9).
+ *@c: int
+ *
+ *Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+int isFalse = 0;
+return ((c >= '0' && c <= '9') ? isFalse = 1 : isFalse);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,5 @@ int _putchar(char c);
 
 /** _isupper declaration*/
 int _isupper(int c);
+/** _isdigit declaration*/
+int _isdigit(int c);
