89b641c94fa9218e9832f17ab3a30716bc2edd4d
chinazaonuoha <developerworkpace@gmail.com>
2024-02-09 08:07:06 +0100

    prints 10 times the numbers, from 0 to 14 fix 0x01

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,38 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *more_numbers - print ten times 0 to 14
+ *more_numbers - prints 10 times the numbers, from 0 to 14
  *
  *Return: Nothing
  */
-
 void more_numbers(void)
 {
-int j = 1;
-char num;
-while (j <= 10)
+int i = 1;
+while (i < 10)
 {
-int i = 0;
-while (i <= 14)
+int j = 0;
+while (j <= 14)
 {
-num = i + '0';
-if (i < 9)
+if (j > 9)
 {
-
-_putchar(num);
-
+_putchar((j / 10) + '0');
 }
-if (i > 9)
-{
-num = '0' + (i / 10);
-_putchar(num);
-num = '0' + (i % 10);
-_putchar(num);
-}
-i++;
-}
-_putchar('\n');
+	  
+_putchar((j % 10) + '0');
 j++;
 }
-
+_putchar('\n');
+i++;
+}
 }
