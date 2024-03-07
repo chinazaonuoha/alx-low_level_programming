422b16094eaf4556dfc655d5bc1a266e01cb3056
chinazaonuoha <developerworkpace@gmail.com>
2024-03-07 16:10:46 +0100

    prints the numbers from 1 to 100 fix 0x03

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,19 @@ int min = 1;
 int max = 100;
 while (min <= max)
 {
-if (min % 3 == 0)
+
+if (min % 3 == 0 && min % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+
+else if (min % 3 == 0)
 {
 printf("Fizz ");
 }
 else if (min % 5 == 0)
 {
 printf("Buzz ");
-}
-else if ((min % 3 == 0) && (min % 5 == 0))
-{
-printf("FizzBuzz ");
 }
 else
 {
